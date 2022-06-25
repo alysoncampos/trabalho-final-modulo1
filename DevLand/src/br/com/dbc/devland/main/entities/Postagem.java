ae6b18ab0a9514fdd02f8a366a05ed316d0a5d1c
Alyson Campos <alyson.siqueiracampos@gmail.com>
2022-06-25 10:49:13 -0300

    Updated class Postagem

--- DevLand/src/br/com/dbc/devland/main/entities/Postagem.java
+++ DevLand/src/br/com/dbc/devland/main/entities/Postagem.java
@@ -1,4 +1,50 @@
 package br.com.dbc.devland.main.entities;
 
+import java.util.Date;
+
 public class Postagem {
+    private Integer id;
+    private String titulo, texto;
+    private Date data;
+    private User usuario;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public User getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(User usuario) {
+        this.usuario = usuario;
+    }
 }
