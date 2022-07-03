ce05518dc447a3d2a6350e260b0b5ff8f6a501dc
Cesar <cesar.desenvolvedor@gmail.com>
2022-07-03 12:09:52 -0300

    Adicao em Repositorio

--- modulo2/src/br/com/dbc/devland/repository/Repositorio.java
+++ modulo2/src/br/com/dbc/devland/repository/Repositorio.java
@@ -7,10 +7,11 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface Repositorio<CHAVE, OBJETO> {
-
     Integer getProximoId(Connection connection) throws SQLException;
 
-    OBJETO adicionar(OBJETO objeto) throws BancoDeDadosException;
+    Integer getProximoIdDev(Connection connection) throws SQLException;
+
+    OBJETO adicionar(OBJETO object) throws BancoDeDadosException;
 
     boolean remover(CHAVE id) throws BancoDeDadosException;
 
